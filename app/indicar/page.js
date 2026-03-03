444004b936017aeb2f047ef23f7497bfabec3b7c
tenexoticas-cmyk <tenexoticas@gmail.com>
2026-03-03 11:37:14 -0300

    Update page.js

--- app/indicar/page.js
+++ app/indicar/page.js
@@ -1,1 +1,8 @@
-
+export default function Indicar() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Fazer uma indicação</h1>
+      <p>Em breve teremos o formulário aqui.</p>
+    </main>
+  );
+}
